dcf630df9447eed5060208b5a2a6677e10340bd6
hxdyxd <hxdyxd@gmail.com>
2015-09-26 12:48:49 +0800

    Update config.php

--- Applications/Shadowsocks/config.php
+++ Applications/Shadowsocks/config.php
@@ -1,9 +1,11 @@
 <?php 
+$SERVER = '127.0.0.1';
 // 加密算法
 $METHOD = 'aes-256-cfb';
-// 密码，www.workerman.net
-$PASSWORD = 'www.workerman.net';
+// 密码
+$PASSWORD = '12345678';
 // 运行后监听哪个端口
-$PORT = 4431;
+$PORT = 443;
+$LOCAL_PORT = 1080;
 // 启动多少进程
 $PROCESS_COUNT = 12;
